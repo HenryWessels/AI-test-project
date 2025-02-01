88ca9368137fd2cf9352f013902427297bed3b28
Henry Wessels <HenrySWessels0099@gmail.com>
2025-02-01 02:30:20 +0200

    Base project files.

---
+++ src/main/java/com/ginger/aitest/rest/controllers/TestController.java
@@ -0,0 +1,9 @@
+package com.ginger.aitest.rest.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    public TestController() {}
+}
---
+++ src/test/java/com/ginger/aitest/AiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ginger.aitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
